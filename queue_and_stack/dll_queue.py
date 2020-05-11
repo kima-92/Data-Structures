d358d84a35b30e8bf4cd307950733b48d72a6856
Wilma <kima_92@fastmail.com>
2020-05-11 18:17:18 -0400

    Added pseudocode to dll_queue.py

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -5,15 +5,20 @@ from doubly_linked_list import DoublyLinkedList
 
 class Queue:
     def __init__(self):
-        self.size = 0
+        # self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
+        # Use DLL's add to head function
+        # It will take care of whether or not it's an empty DLL
         pass
 
     def dequeue(self):
+        # Use DDL's remove from tail function
+        # It will take care of whether or not it's an empty DLL
         pass
 
     def len(self):
+        # Return the length of the DLL
         pass
